c8f11d2264a4e495266d45e8868b5bbe94ea21a2
Thembelihle <thembelihlengobese3@gmail.com>
2023-07-28 16:29:10 +0200

    5-print changed

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -2,5 +2,8 @@
 for x in range(0,100):
     if x < 10:
         print("0{},".format(x), end=" ")
+    elif x == 99:
+         print("{}".format(x))
     else:
         print("{},".format(x), end=" ")
+
