b1a3c90d0a6d9a779636b92518954d539e9b3102
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-01 13:08:42 +0200

    comb-2

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,8 +3,6 @@
 for x in range(0,100):
     if x < 10:
         print("0{},".format(x), end=" ")
-    elif x == 99:
-        print("{}".format(x))
     else:
         print("{},".format(x), end=" ")
 
