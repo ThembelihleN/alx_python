3b8f938c6406fcb267867b6cb00a661fb17e3500
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-03 14:36:22 +0200

    modified

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,22 +1,18 @@
-'''letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '@', '&', '(', ')', '*', '+' ]
- characters = letters + numbers + symbols
-    password = " ".join(characters)'''
+import re
 def validate_password(password):
-    '''for i in range(8):
-        return password'''
-    if len(password) >= 8:
-        return True
-    else:
+   
+    if len(password) < 8:
         return False
+    if not re.search('[a-z]', password):
+        return  False
+    if not re.search('[A-Z]', password):
+        return  False
+    if not re.search('[0-9]', password):
+        return  False
+    else:
+        return True
     
-validate_password("Password123")
+(validate_password("Password123"))
 validate_password("abc123")
 validate_password("Password 123")
 validate_password("password 123")
