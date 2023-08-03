109ac77d452e6852526d34ae8a3cb2471db7c072
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-03 15:35:01 +0200

    changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
-#from 0-add.py import def add(a, b):
-a = 1
-b = 2
+if __name__ == "__main__":
+    import math
+    a = 1
+    b = 2
 def add(a, b):
-     print("1 + 2 = {}".format (a + b))
+    print("1 + 2 = {}".format (a + b))
 
 add(1, 2)
