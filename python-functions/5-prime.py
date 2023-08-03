b0567df44c6b9de1554a19ccdc92e53cc75c0e58
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-03 14:05:43 +0200

    added files

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,14 +1,6 @@
 def is_prime(number):
-    if number > 1:
-        for i in range(2, (number/2) + 1):
-            if (number % i) == 0:
-                break
-            else:
-                print("True")
-        else:
-            print("False")
-
-is_prime(17)
+    i = 2
+    
 is_prime(15)
 is_prime(-5)
 is_prime(0)
