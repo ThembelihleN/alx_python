3990bce56b72dbcef8af94fcd4ebd09579b82d87
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-04 15:54:42 +0200

    raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,6 +1,5 @@
 def raise_exception():
-    print("")
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
