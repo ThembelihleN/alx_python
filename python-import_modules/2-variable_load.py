42905f51b2f8b1ec42db934fe73475168babb2e8
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-04 15:40:32 +0200

    script

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,5 @@
 #Write a program that imports the variable a from the file variable_load_2.py and prints its value.
-from variable_load_2 import a
+if __name__ == "__main__":
+    from variable_load_2 import a
 
 print(a)
