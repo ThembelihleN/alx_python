5be9e469d41102e5c70a153a29b892e8773b5854
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-04 14:45:02 +0200

    changed file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 a = 1
 b = 2
 if __name__ == "__main__":
-    from add_0 import add
+    from math import add
+def add(a, b):
     print("1 + 2 = {}".format("a + b"))
 
 add(1, 2)
--- python-import_modules/add_0.py
+++
@@ -1,7 +0,0 @@
-#python 3
-def add(a, b):
-   """FAKE!
-    """
-   return (a - b)
-
-    
