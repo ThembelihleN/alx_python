deb8d047d1b700cc5ffa79513846115972a964b7
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-08 17:34:50 +0200

    safe print

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,6 +1,19 @@
 #Write a function that divides 2 integers and prints the result
-if __name__ == "__main__":
-    from variable_load_2 import a
+#if __name__ == "__main__":
+#    from variable_load_2 import a
 
 def safe_print_division(a, b):
-    print("{}".format())
+    #print("{}".format())
+    try:
+        a = 12
+        b = 2
+
+        result = a/b
+        print("Inside result: {:d} / {:d} = {}".format(a, b, result))
+        
+    except:
+        print("Error: Denominator cannot be 0.")
+
+    finally:
+        result = safe_print_division(a, b)
+        print("Inside result: {:d} / {:d} = {}".format(a, b, result))
