c0657fceed6799efddf4d830b9afb4de34feda7b
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-12 12:10:35 +0200

    files

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,11 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+def print_matrix_integer(matrix=[[]]):
+    for item in row:
+        for row in matrix:
+            print("{}{}{}$".format()()())
+
+print_matrix_integer(matrix)
---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,7 @@
+length = len()
+first = [0]
+def multiple_returns(sentence):
+    if sentence == " ":
+        [0] == None
+    else:
+        print("Length: {:d} - First character: {}".format(length, first))
