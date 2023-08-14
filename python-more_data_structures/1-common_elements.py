f1ef93757b694c1def417ba199292cec695349b6
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-14 16:56:42 +0200

    common elements

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,7 @@
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+
+def common_elements(set_1, set_2):
+    print(sorted(list(c_set)))
+
+c_set = common_elements(set_1, set_2)
