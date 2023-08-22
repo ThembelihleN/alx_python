b6969bd0a66216770893f789156ceacd658c4949
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-22 10:44:26 +0200

    No C

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -4,7 +4,3 @@
 def no_c(my_string):
     new_string = print(my_string.translate({ord(i):None for i in 'Cc'}))
     return new_string
-  
-no_c("Holberton School")
-no_c("Chicago")
-no_c("C is fun!")
