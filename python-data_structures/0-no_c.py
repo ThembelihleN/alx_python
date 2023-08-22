f0ebee7a7a10d3fca80ab116b50f8b449e1a83a5
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-22 10:30:43 +0200

    Can you C me now

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,9 +2,9 @@
 #print(string.translate({ord('c'): None}))
 #print(string.replace('c', ''))
 def no_c(my_string):
-    new_string = print(my_string.translate({ord('c'): None}))
+    new_string = print(my_string.translate({ord(i):None for i in 'Cc'}))
     return new_string
   
-no_c("Holberton School")
-#print("Chicago")
-#print("C is fun!")
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
