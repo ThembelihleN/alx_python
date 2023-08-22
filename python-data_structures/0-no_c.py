1f2bccd089c41c28dde9325a235555a805f8402b
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-22 09:53:48 +0200

    no C

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
-    new_string = my_string.translate({ord('c'):None})
-    return my_string
+    new_string = "".join(c for c in my_string if c.isalpha())
+    print(new_string)
 
 
 print("Holberton School")
