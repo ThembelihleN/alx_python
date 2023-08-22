e6cae20c3fba9a1cb516eaa630b8723df41a9a76
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-22 10:24:04 +0200

    no c

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,10 @@
+#string = "Holberton School"
+#print(string.translate({ord('c'): None}))
+#print(string.replace('c', ''))
 def no_c(my_string):
-    print(my_string.translate({ord('c'): None}))
-
-
-print("Holberton School")
-print("Chicago")
-print("C is fun!")
+    new_string = print(my_string.translate({ord('c'): None}))
+    return new_string
+  
+no_c("Holberton School")
+#print("Chicago")
+#print("C is fun!")
