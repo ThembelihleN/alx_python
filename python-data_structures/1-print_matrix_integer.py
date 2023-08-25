6197f929605ebed3c078e726ea17aee46b0b1e03
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-25 16:15:16 +0200

    join method

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,11 @@
 #my_list = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
-
-matrix = [[i for i in range(3)] for _ in range(3)]
+my_list = [[i for i in range(3)] for _ in range(3)]
 def print_matrix_integer(matrix=[[]]):
+    matrix = ", ".join(str(x) for x in my_list)
     print("{:d}{:d}{:d}$".format((matrix)))
 
+
+#my_list = [1, 2, 3]
+#my_string = ", ".join(str(x) for x in my_list)
+#print("My list contains: {}".format(my_string))
 #print_matrix_integer(matrix)
