9297badafa2801f23b3f6a8315cd9928af070241
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-25 15:51:49 +0200

    nested comprehensions

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
-my_list = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
+#my_list = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
 
+matrix = [[i for i in range(3)] for _ in range(3)]
 def print_matrix_integer(matrix=[[]]):
-    for i in my_list:
-        print("{:d}{:d}{:d}$".format(str([*my_list])))
+    print("{:d}{:d}{:d}$".format((matrix)))
 
 #print_matrix_integer(matrix)
