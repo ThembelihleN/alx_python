fa554c7490b46845bde43b38e032cef84aec26cb
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-29 18:32:14 +0200

    c is Fun

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,12 +1,25 @@
+"""
+Flask is a Python module that lets you develop web applications
+easily.
+"""
 from flask import Flask
-'''
-This is a documentation for the flask web application. 
-'''
+
 app = Flask(__name__)
+"""
+ a route refers to a URL pattern that is defined within the framework to determine 
+ how incoming requests are handled.
+"""
+@app.route("/")
+def hello():
+    """
+    This is a function that returns a specified string when routing to /
+    """
+    return "Hello HBNB!"
 
 @app.route("/hbnb")
 def hbnb():
     return "HBNB"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
