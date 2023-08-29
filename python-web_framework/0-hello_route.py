d9767a834375b964140e6912364a843e12e1f434
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-29 17:13:59 +0200

    first commit

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,13 +1,17 @@
-from flask import Flask
 '''
 Flask is a web framework, it is a Python module that lets you develop web applications easily.
 It has a small and easy-to-extend core: it is a microframework that doesn’t include an ORM 
 (Object Relational Manager) or such features.
 '''
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """
+    This is a function that returns a specifies string when routing to /
+    """
     return "Hello HBNB!"
 
 
