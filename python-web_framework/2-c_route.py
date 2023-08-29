520e2d6532db0f4ab9c02958f1f335df9bb33d20
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-29 20:00:56 +0200

    Route

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -3,6 +3,13 @@ Flask is a Python module that lets you develop web applications
 easily.
 """
 from flask import Flask
+"""
+The escape function is used for safely escaping HTML and other
+special characters in order to prevent cross-site scripting (XSS)
+attacks and ensure that user-generated content is displayed as
+plain text rather than being interpreted as HTML or script code.
+"""
+from markupsafe import escape
 
 app = Flask(__name__)
 """
