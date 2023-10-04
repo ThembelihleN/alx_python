2136fa186a891103b85b38322ede7e1816ec9175
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-04 20:20:09 +0200

    updated

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,4 +85,42 @@ class Rectangle(Base):
        """
        self.__y = value
 
-
+    def area(self):
+        """Area method: returns width * height"""
+        return self.__width * self.__height
+    
+    def display(self):
+        """Display the rectangle using char #."""
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+    
+    def __str__(self):
+        """Returns a formatted output."""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+    
+    def update(self, *args, **kwargs):
+        """Update attributes using *args."""
+        if args and len(args) > 0:
+            self.id = args[0]
+        if args and len(args) > 1:
+            self.width = args[1]
+        if args and len(args) > 2:
+            self.height = args[2]
+        if args and len(args) > 3:
+            self.x = args[3]
+        if args and len(args) > 4:
+             self.y = args[4]
+        
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            elif key == "width":
+                self.width = value
+            elif key == "height":
+                self.height = value
+            elif key == "x":
+                self.x = value
+            elif key == "y":
+                self.y = value
