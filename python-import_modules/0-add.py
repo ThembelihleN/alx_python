9f6135c5378d651f9f91f850d5c5c3122bd0912b
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-04 19:34:36 +0200

    updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,5 +2,5 @@ if __name__ == "__main__":
     from add_0 import add
 a = 1
 b = 2
-result = add(a+b)
-print("1 + 2 = {}".format("add" ,(result)))
+result = add(a, b)
+print("{} + {} = {}".format("add" ,(result)))
--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,9 +1,23 @@
 #Write a program that prints the number of and the list of its arguments.
-if __name__ == "__main__":
-    import sys 
+import sys
 
-print(len(sys.argv), "arguments:")
-del sys.argv[0]
-print(sys.argv)
-for i in sys.argv:
-    print(i)
+# defining my function
+def main():
+    arguments = sys.argv[1:]
+    argument_number = len(arguments)
+
+    print("{}".format(argument_number), end=" ")
+    if argument_number == 1:
+        print("argument:")
+    elif argument_number == 0:
+        print("arguments", end=".\n")
+        return
+    else:
+        print("arguments:")
+
+    for num, arg in enumerate(arguments, 1):
+        print("{}: {}".format(num, arg))
+
+
+if __name__ == "__main__":
+    main()
