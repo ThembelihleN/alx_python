b5ca4d6ddd6c7e82587ba0caa136d002be942b17
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-04 19:08:35 +0200

    square

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+Method:
+    area: Raises an exeption.
+"""
+
+
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__()\
+                if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+    Do nothing: By passing pass.
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__()\
+                if attribute != '__init_subclass__']
+    """
+    area: takes in no agument other than self.
+    """
+    def area(self):
+        """
+        Raises an exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates the input.
+        Attributes:
+            name(string): the name string.
+            value(int): must be an integer greater than 0.
+        """
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class
+    """
+    def __init__(self, width, height):
+        """
+        attributes:
+            width: int
+            height: int
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        area method: returns width * height
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        return a formated output
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """
+    Squade class.
+    Method:
+        area: calculate the area of a square based on the size.
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
