6070280db9c17cbb954f1e9574b7b51923fa556a
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-04 19:00:46 +0200

    edited

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -6,5 +6,9 @@ class BaseGeometry:
     """
     class description
     """
+    def __dir__(self):
+        attributes = super().__dir__()
+        return [i for i in attributes if i != '__init_subclass__']
+    
     def area(self):
         raise Exception('area() is not implemented')
--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,6 +6,10 @@ class BaseGeometry:
     """
     class description
     """
+    def __dir__(self):
+        attributes = super().__dir__()
+        return [i for i in attributes if i != '__init_subclass__']
+    
     def area(self):
         raise Exception('area() is not implemented')
     
