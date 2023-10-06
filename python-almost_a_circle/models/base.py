f679ebb62eb1379853bcdcd738b4907e24989a0d
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-06 06:58:22 +0200

    documented

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,25 @@
-"""
-module documentation
-"""
+'''
+Base Module: this module contains the base class of all other classes in the project.
+class:
+    Base: created the base class.
+'''
+
 
 class Base:
+    """
+    Base class Method:
+        __init__: Initialises the class with id(int),
+                  Increment __nb_objects.
+    Attributes:
+        __nb_objects: Initial value = 0
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''
+        Increments __nb_objects
+        id(int): integer value.
+        '''
         if id is not None:
             self.id = id
         else:
