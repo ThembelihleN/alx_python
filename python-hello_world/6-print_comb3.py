7cbea3e77d4288732ee69a9a4e04d2ef4c60119d
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-06 06:48:04 +0200

    last change

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,15 +1,5 @@
 #all possible different combinations of two digits
 
-#for i in range(0,90):
-#    for x in range(0,90):
-#        if x < 10:
-#            print("0{},".format(x), end=" ")
-#        elif x == 89:
-#            print("{}".format(x))
-#        else:
-#            print("{},".format(x), end=" ")
-
-
 for i in range(10):
     for j in range(i +1, 10):
         if i == 8 and j == 9:
