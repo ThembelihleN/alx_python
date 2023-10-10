ddb9d968240589490df30bc227bd294ad3e6ff46
Thembelihle <thembelihlengobese3@gmail.com>
2023-10-10 13:43:51 +0200

    changed

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,25 @@
 #my_list = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
-my_list = [[i for i in range(3)] for _ in range(3)]
+#my_list = [[i for i in range(3)] for _ in range(3)]
+#def print_matrix_integer(matrix=[[]]):
+   # matrix = ", ".join(x for x in my_list)
+   # print("{:d}{:d}{:d}$".format((matrix)))
+
 def print_matrix_integer(matrix=[[]]):
-    matrix = ", ".join(x for x in my_list)
-    print("{:d}{:d}{:d}$".format((matrix)))
-
-
-#my_list = [1, 2, 3]
-#my_string = ", ".join(str(x) for x in my_list)
-#print("My list contains: {}".format(my_string))
-#print_matrix_integer(matrix)
+    # if matrix is empty, just print an empty line and return 
+    if not matrix:
+        print()
+        return
+    # iterate over each row in the matrix
+    for row in matrix:
+        # if row is empty print a new line and skip to next row 
+        if not row:
+            print()
+            continue
+        # iterate over each element in row using enumerate
+        for i, num in enumerate(row):
+            # if it is the last element in row print element folowed by a new line 
+            if i == len(row) -1:
+                print("{:d}".format(num))
+            # else if it is not the last print element followed by a space
+            else:
+                print("{:d}".format(num), end=" ")
--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,7 +1,13 @@
-length = len()
-first = [0]
+#length = len()
+#first = [0]
+#def multiple_returns(sentence):
+#    if sentence == " ":
+#        [0] == None
+#    else:
+#        print("Length: {:d} - First character: {}".format(length, first))
+
+
 def multiple_returns(sentence):
-    if sentence == " ":
-        [0] == None
-    else:
-        print("Length: {:d} - First character: {}".format(length, first))
+    lenght = len(sentence)
+    char = sentence[0] if lenght > 0 else None
+    return lenght, char
